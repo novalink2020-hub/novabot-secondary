f8e9171b8d7b1aef787ee25fd043dbc35c3f6c80
novalink2020-hub <novalink2020@gmail.com>
2026-01-05 01:53:38 +0200

    Update novabot-loader.js

--- novabot-loader.js
+++ novabot-loader.js
@@ -1596,7 +1596,7 @@ async function dispatchNovaLeadEvent(payload) {
   setTimeout(() => {
     addStaticBotMessage("");
     showCardByType("business_subscribe");
-  }, 3000);
+  }, 5000);
 }
 
       chatHistory.push({ role: "assistant", content: replyText });
